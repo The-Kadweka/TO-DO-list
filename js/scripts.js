20011e849c33d7b54dd50351a5c5cdd6c606d39d
hussein18149 <husseinkatana17@gmail.com>
2020-01-28 16:23:57 +0300

    added an input form and a button to add the items

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('add').addEventListener('click', add);
+show();
+
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str != null) {
+        todos = JSON.parse(todos_str); 
+    }
+    return todos;
+}
