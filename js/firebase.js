5c845683e54c2a487249ddb559969129427fa174
hussein18149 <husseinkatana17@gmail.com>
2020-01-30 22:25:50 +0300

    add config objects

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,10 @@
-var firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appId: "app-id",
-    measurementId: "G-measurement-id",
+const firebaseConfig = {
+    apiKey: "AIzaSyB18jby2mVB1eHNp836t9lfurYEt_fEBjk",
+    authDomain: "do-list-18619.firebaseapp.com",
+    databaseURL: "https://do-list-18619.firebaseio.com",
+    projectId: "do-list-18619",
+    storageBucket: "do-list-18619.appspot.com",
+    messagingSenderId: "934288992029",
+    appId: "1:934288992029:web:5c5ef3236933355296e3f4",
+    measurementId: "G-YBJ4Q1JVLP"
   };
